0ef7e6f96c3e5b32a634f8e307d7dd7b52366b00
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-03-22 17:08:23 +0300

    Add postcss.config.js and update App.tsx, Navbar.tsx, and main.tsx and create ProductList and ProductDetails and add Hook folder abut react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App";
+import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const container = document.getElementById("root");
+if (container !== null) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root container not found");
+}
