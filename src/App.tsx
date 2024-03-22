f6f6bcd8964bcede149b3f78bb1a668aa83f818a
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-03-22 23:02:14 +0300

    Update HTML and add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
 import Navbar from "./components/Navbar";
+import FavoritesPage from "./pages/FavoritesPage";
+import ShoppingCartPage from "./pages/ShoppingCartPage";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/cart" element={<ShoppingCartPage />} />
         </Routes>
       </div>
     </Router>
