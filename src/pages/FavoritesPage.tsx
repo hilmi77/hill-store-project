48992b99aab65f1ce8ac1e8ab531d4f3aa70ebde
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-03-23 15:20:13 +0300

    Remove unused imports from FavoritesPage and ShoppingCartPage

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeFavorite } from "../features/favorites/favoritesSlice";
 import { RootState } from "../app/store";
--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ShoppingCartPage = () => {
   return <div>ShoppingCartPage</div>;
 };
